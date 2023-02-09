f49f8208fa38b9e151d729f43ef398aa3a56c4d6
Mihajlo Nikolic <mihajlo.nikolic.991@gmail.com>
2023-02-09 22:07:06 +0100

    added new env for render

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  API_HOST: 'https://pass-keep-api.herokuapp.com/api/v1/',
-  API_DOMAIN: 'https://pass-keep-api.herokuapp.com/'
+  API_HOST: 'https://pass-keep.onrender.com/api/v1/',
+  API_DOMAIN: 'https://pass-keep.onrender.com/'
 };
